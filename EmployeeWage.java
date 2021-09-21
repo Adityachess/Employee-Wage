a7b02e3fd128b4697499844ce81ba234c1ee7049
Adityachess <adityadone9398@gmail.com>
2021-09-21 12:30:07 +0530

    Employee Attendance using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,23 +7,18 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Programme");
-		
-		int wageperhr=20;
-		int parttimehrs=4;
-		int dailyhrs=8;
-		int fulltimewage= wageperhr * dailyhrs;
-		int parttimewage=wageperhr * parttimehrs;
-		
+
 		Random ran = new Random();
-		int empCheck= ran.nextInt(2);
-		System.out.println(empCheck);
-		if(empCheck==1) {
-			System.out.println("Employee is Parttime ="+ parttimewage);
-		}
-		else
-		System.out.println("Employee fulltime wage = "+fulltimewage);
 		
+		int empCheck = ran.nextInt(2);
+		
+		switch (empCheck) {
+		case 0:
+			System.out.println("Employee is Present.");
+			break;
 
+		default:
+			System.out.println("Employee is Absent");
+		}
 	}
-
 }
