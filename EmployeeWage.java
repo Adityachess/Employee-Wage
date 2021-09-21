299d0a5f9586a52a47f8f6110a16e156e812224b
Adityachess <adityadone9398@gmail.com>
2021-09-21 12:13:54 +0530

    Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,22 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Programme");
+		
+		Random ran = new Random();
+		int empCheck= ran.nextInt(2);
+		System.out.println(empCheck);
+		if(empCheck==0) {
+			System.out.println("Employee is Present");
+		}
+		else {	
+			System.out.println("Employee is Absent");
+		}
 
 	}
 
