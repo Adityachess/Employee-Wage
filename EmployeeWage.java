d43bf84f7fc5ebb47bcf1669e3f486b601ed4d46
Adityachess <adityadone9398@gmail.com>
2021-09-21 12:47:05 +0530

    Calculating total working days and toal employee wage for defined hours

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,15 +6,52 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int dayspermnth = 20;
-		int wageperhr = 20;
-		int dailyhr = 8;
-		int dailywage = wageperhr * dailyhr;
+		int Total_hurs = 100;
+		int Total_days = 20;
+		int wage_hurs = 20;
+		int daily_hurs = 8;
 
-		System.out.println("Employee Daily Wage =" + dailywage);
+		int empHr;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
 
-		int monthlywage = dailywage * dayspermnth;
+		System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
 
-		System.out.println("Monthly Wage Of Employee is =" + monthlywage);
+		while (totalEmpHrs <= Total_hurs && totalWorkingDays < Total_days) {
+			totalWorkingDays++;
+
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+			System.out.println("Random Integers: " + empCheck);
+
+			switch (empCheck) {
+			case 1:
+				empHr = 8;
+				System.out.print("full time");
+				break;
+			case 2:
+				empHr = 4;
+				System.out.print("part time");
+				break;
+			case 0:
+				empHr = 0;
+				System.out.println("absent");
+				break;
+
+			default:
+				empHr = 0;
+				break;
+			}
+
+			empWage = empHr * wage_hurs;
+
+			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("days" + totalWorkingDays + "emphrs" + empHr);
+			System.out.println("wage" + totalEmpWage);
+			System.out.println("total wage" + totalEmpWage);
+		}
+
 	}
 }
