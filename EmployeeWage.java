bc3a1e57629389d18289cc327608b216b620902c
Adityachess <adityadone9398@gmail.com>
2021-09-21 12:26:45 +0530

    fulltime and part time wage calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,23 +9,19 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Programme");
 		
 		int wageperhr=20;
+		int parttimehrs=4;
 		int dailyhrs=8;
+		int fulltimewage= wageperhr * dailyhrs;
+		int parttimewage=wageperhr * parttimehrs;
 		
 		Random ran = new Random();
 		int empCheck= ran.nextInt(2);
 		System.out.println(empCheck);
-		if(empCheck==0) {
-			System.out.println("Employee is Present");
-			
-			int dailywage= wageperhr * dailyhrs;
-			
-			System.out.println("Employee Daily wage = "+dailywage);
-
+		if(empCheck==1) {
+			System.out.println("Employee is Parttime ="+ parttimewage);
 		}
-		else {	
-			System.out.println("Employee is Absent");
-		}
-		
+		else
+		System.out.println("Employee fulltime wage = "+fulltimewage);
 		
 
 	}
