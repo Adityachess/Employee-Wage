454986bed031817799e3a9647279f5e8fb1f637f
Adityachess <adityadone9398@gmail.com>
2021-09-21 12:19:31 +0530

    Calculation of Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,15 +8,25 @@ public class EmployeeWage {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Programme");
 		
+		int wageperhr=20;
+		int dailyhrs=8;
+		
 		Random ran = new Random();
 		int empCheck= ran.nextInt(2);
 		System.out.println(empCheck);
 		if(empCheck==0) {
 			System.out.println("Employee is Present");
+			
+			int dailywage= wageperhr * dailyhrs;
+			
+			System.out.println("Employee Daily wage = "+dailywage);
+
 		}
 		else {	
 			System.out.println("Employee is Absent");
 		}
+		
+		
 
 	}
 
